cef1a4a9865b6d30c2d1403f6d61edb415e5b537
Pawel <paweljozwiak1986@gmail.com>
2019-02-22 10:58:01 +0100

    grabbing clock hands elements

---
+++ zegar.js
@@ -0,0 +1,6 @@
+const hours = document.querySelector(".hours__hand");
+const minutes = document.querySelector(".minutes__hand");
+const seconds = document.querySelector(".seconds__hand");
+
+
+console.log(hours); 
