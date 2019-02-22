5b39714866aa1b6992cf11d44a3392a54652ea2c
Pawel <paweljozwiak1986@gmail.com>
2019-02-22 12:01:10 +0100

    clock done

--- zegar.js
+++ zegar.js
@@ -1,6 +1,21 @@
-const hours = document.querySelector(".hours__hand");
-const minutes = document.querySelector(".minutes__hand");
-const seconds = document.querySelector(".seconds__hand");
+setInterval(function(){
+const  date = new Date();
+  updateTime(date);
+  updateClock(date);
+}, 1000);
+
+const updateClock = (date) => {
+  const hours = document.querySelector(".hours__hand").style;
+  const minutes = document.querySelector(".minutes__hand").style;
+  const seconds = document.querySelector(".seconds__hand").style;
 
 
-console.log(hours); 
+  seconds.transform = "rotate(" + date.getSeconds() * 6 + "deg)";
+  minutes.transform = "rotate(" + date.getMinutes() * 6 + "deg)";
+  hours.transform = "rotate(" + (date.getHours() * 30 + date.getMinutes() * 0.5) + "deg)";
+}
+
+const updateTime = (date) => {
+  const timeDiv = document.querySelector(".innner__clock");
+  const time = date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds();
+};
